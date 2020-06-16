5a23d4953c88b50903f3510f8b513787223bc7bf
Haishan Shi <53292938+haishan-shi@users.noreply.github.com>
2020-06-16 22:26:43 +1000

    Delete convert.py

--- convert.py
+++
@@ -1,8 +0,0 @@
-#simple script to convert all .png files to .jpg
-from glob import glob                                                           
-import cv2 
-pngs = glob('./*.png')
-
-for j in pngs:
-    img = cv2.imread(j)
-    cv2.imwrite(j[:-3] + 'jpg', img)
